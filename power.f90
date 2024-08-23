be96bbad2b038de79662b56712a00e06292119bd
Ameeya Bhusan Sahoo <167878147+ameeya-bhusan@users.noreply.github.com>
2024-08-23 18:41:26 +0530

    Create power.f90

---
+++ power.f90
@@ -0,0 +1,11 @@
+program power
+      !To find the value of one number raised to the power of another
+      implicit none
+      integer :: x,y,z
+      write (*,*) 'please enter the value of x: '
+      read (*,*) x
+      write (*,*) 'please enter the value of y: '
+      read (*,*) y
+      z = x**Y
+      write (*,*) 'the answer is = ', z
+      end program power
