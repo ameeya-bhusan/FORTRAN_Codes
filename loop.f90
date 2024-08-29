28d0e95a954d9bda9b1e53d1d31bb5f29b832aec
Ameeya Bhusan Sahoo <167878147+ameeya-bhusan@users.noreply.github.com>
2024-08-29 18:42:16 +0530

    Create loop.f90

---
+++ loop.f90
@@ -0,0 +1,19 @@
+program loop
+      !purpose:::::::::::::::::::::::::::::::::::::::
+      !to generate the all combinations of 1,2 and 3
+      implicit none
+      integer :: i
+      integer :: j
+      integer :: k
+     !++++++++++++++++++++++++++++++++++++++++++++++++++++
+      outer: do i = 1,3
+      middle: do j = 1, 3
+      inner: do k = 1,3
+      if (i .ne. j .and. j .ne. k .and. k .ne. i) then
+              write (*,100) i, j, k
+              100 format (3i3)
+      end if
+      end do inner
+      end do middle
+      end do outer
+      end program loop
